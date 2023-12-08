1528b474b4565372870a74cff7dfe2b328f64186
Sylvia T <sylviarosemarie@gmail.com>
2023-12-08 15:03:55 -0600

    Completed initial model setup and migration

--- playlistapi/models/episodetag.py
+++ playlistapi/models/episodetag.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class EpisodeTag(models.Model):
     tag = models.ForeignKey("Tag", on_delete=models.CASCADE, related_name='tag')
-    episode = models.ForeignKey("Episode", on_delete=models.CASCADE, related_name='episode')
+    episode = models.ForeignKey("Episode", on_delete=models.CASCADE, related_name='episode_tag')
