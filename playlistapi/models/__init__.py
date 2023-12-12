3a1fb126037749035fb200f08cde251441aef5d8
Sylvia T <sylviarosemarie@gmail.com>
2023-12-12 16:01:11 -0600

    Started views for each model and imported each to the init

--- playlistapi/models/__init__.py
+++ playlistapi/models/__init__.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from .creator import Creator
 from .friend import Friend
 from .episode import Episode
---
+++ playlistapi/views/playlist_view.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+
+class PlaylistView(ViewSet):
